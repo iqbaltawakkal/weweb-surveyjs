ab4b0381d4056f860dd7a4337b281db958b1734f
Iqbal Tawakkal <iqbaltawakkal1@gmail.com>
2024-10-04 18:35:46 +0700

    first commit

---
+++ src/components/SurveyCreator.vue
@@ -0,0 +1,31 @@
+<template>
+  <SurveyCreatorComponent :model="creator" />
+</template>
+
+<script setup>
+import { SurveyCreatorModel } from 'survey-creator-core'
+import { SurveyCreatorComponent } from 'survey-creator-vue'
+import 'survey-core/survey.i18n.js'
+import 'survey-creator-core/survey-creator-core.i18n.js'
+import 'survey-core/defaultV2.min.css'
+import 'survey-creator-core/survey-creator-core.min.css'
+
+class SurveyTemplatesTabPlugin {
+  constructor(creator) {
+    this.model = creator
+  }
+
+  // Do nothing when the tab is activated or deactivated
+  activate() { }
+  deactivate() {
+    return true
+  }
+}
+
+const options = {
+  showLogicTab: true,
+  showJSONEditorTab: true
+}
+const creator = new SurveyCreatorModel(options)
+new SurveyTemplatesTabPlugin(creator)
+</script>
---
+++ src/wwElement.vue
@@ -0,0 +1,14 @@
+<template>
+  <SurveyCreator :key="updateComponent" />
+</template>
+
+<script setup>
+import SurveyCreator from './components/SurveyCreator.vue'
+import { watch, ref } from 'vue'
+const props = defineProps({
+  content: { type: Object, required: true },
+})
+
+const updateComponent = ref(0);
+watch(() => props.content, () => (updateComponent.value += 1), { deep: true });
+</script>
